4538782a4f30c227f9057efd8bd6bba5ebf56368
yoi372323-design <yoi372323@gmail.com>
2026-08-15 07:28:23 +0900

    ping.js

---
+++ commands/ping.js
@@ -0,0 +1,9 @@
+// commands/ping.js
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder().setName('ping').setDescription('Pong!'),
+  async execute(interaction) {
+    await interaction.reply('Pong! 🏓');
+  }
+};
